2cb941c13aeb98431ec846518b7ec55d6b72cda0
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-02-07 01:27:51 +0530

    first commit

---
+++ Array/Youtube/searchEle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class searchEle {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+
+        Scanner sc = new Scanner(System.in);
+        int ele = sc.nextInt();
+
+        for(int i = 0; i<arr.length; i++){
+            if(ele == arr[i]){
+                System.out.println("element found at index "+i);
+            }
+        }
+
+        
+
+        sc.close();
+    }
+}
