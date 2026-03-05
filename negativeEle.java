5af67e5a7f3d14c95934c43d66fc7863297a00c5
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-06 00:46:44 +0530

    Printing negative elements only

---
+++ negativeEle.java
@@ -0,0 +1,12 @@
+public class negativeEle {
+    public static void main(String[] args) {
+        int[] arr = {12, -5, 0, 8, -22, -1, 45, -9};
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < 0){
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+    }
+}
