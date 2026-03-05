d65a9364faaf6b9fcf8ffa87762ce39b7396c78f
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-06 01:13:19 +0530

    Smallest element in an array

---
+++ smallestElement.java
@@ -0,0 +1,14 @@
+public class smallestElement {
+    public static void main(String[] args) {
+        int[] arr = {100, 60, 50, 80, 30, 90};
+
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest element in an array : "+min);
+    }
+}
