6545a09cf84bcea5b8b982bdb3a5e02402ca2057
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-06 01:05:09 +0530

    Product of all elements in an array

---
+++ elementsProduct.java
@@ -0,0 +1,13 @@
+public class elementsProduct {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        int product = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
+        }
+
+        System.out.println("Product of elements in array : "+ product);
+    }
+}
