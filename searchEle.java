a457e6b0d3aa1baaa966f67de95e098c232f9441
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-05 23:46:16 +0530

    re organized

--- searchEle.java
+++ searchEle.java
@@ -7,6 +7,7 @@ public class searchEle {
         Scanner sc = new Scanner(System.in);
         int ele = sc.nextInt();
 
+        
         for(int i = 0; i<arr.length; i++){
             if(ele == arr[i]){
                 System.out.println("element found at index "+i);
