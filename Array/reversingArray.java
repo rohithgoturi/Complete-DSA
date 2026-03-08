0af4b09f2f4177ef79bdc8b87508667fd94991f2
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-08 12:30:49 +0530

    reversing an array

---
+++ Array/reversingArray.java
@@ -0,0 +1,21 @@
+public class reversingArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        int n = arr.length;
+        int i = 0, j = n - 1;
+
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
