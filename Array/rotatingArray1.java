9c6a9a98433e6a5cca027ef0256cb333e8bbb730
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-09 23:02:51 +0530

    rotating an array

---
+++ Array/rotatingArray1.java
@@ -0,0 +1,16 @@
+public class rotatingArray1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
+
+        int n = arr.length;
+        int [] arr2 = new int[n];
+
+        for (int i = k; i < arr.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                
+            }
+        }
+    }
+
+}
