812ec3d81156cca6a8119a4a21901150af887d8e
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-10 23:29:00 +0530

    wave array

---
+++ Array/waveArray.java
@@ -0,0 +1,16 @@
+public class waveArray {
+    // we are given a sorted array and we have to convert into wavy array i. e arr[0]>= arr [1] <= arr [2].. 
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        for (int i = 0; i < arr.length - 1; i+=2) {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+
+        for (int ele : arr) {
+            System.out.print(ele+ " ");
+        }
+    }
+}
