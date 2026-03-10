590c4718b952c345173843ed2b34d1854a6f10aa
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-10 18:46:17 +0530

    Segregating 0s and 1s method-1

---
+++ Array/segregate0sand1sA.java
@@ -0,0 +1,22 @@
+public class segregate0sand1sA {
+    public static void main(String[] args) {
+        int [] arr = {0, 0, 1, 1, 0};
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while(i<j) {
+            if(arr[i] == 0) i++;
+            else if (arr[j] == 1) j--;
+            else{
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        for(int ele : arr) {
+            System.out.print(ele+ " ");
+        }
+    }
+}
