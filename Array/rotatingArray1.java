d1d76bc0c309500ad13d736a67398621cf3084ff
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-10 11:30:07 +0530

    rotating a part of array

--- Array/rotatingArray1.java
+++ Array/rotatingArray1.java
@@ -1,16 +1,25 @@
 public class rotatingArray1 {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        int k = 2;
-
         int n = arr.length;
-        int [] arr2 = new int[n];
+        int k = 2;
+        k = k%n;
 
-        for (int i = k; i < arr.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                
-            }
+        int i = 0, j = k-1;
+        while(i<j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
         }
+
+        for(int ele : arr) {
+            System.out.print(ele+" ");
+        }
+
+        
     }
 
 }
