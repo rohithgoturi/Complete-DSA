35b6562a9ac0a61d8074e4e2a12b9f54637294c5
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-14 13:58:33 +0530

    imserting in ArrayList

---
+++ Array/insertArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class insertArrayList {
+    public static void main(String[] args) {
+     ArrayList<Integer> arr = new ArrayList<>();
+
+     arr.add(10);
+     arr.add(20);
+     arr.add(30);
+     arr.add(1,100);
+     System.out.println(arr);
+
+    }
+}
