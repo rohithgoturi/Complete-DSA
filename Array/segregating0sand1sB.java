d129987a560f28659f08fe839cefac6a2d172a36
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-03-19 23:29:30 +0530

    segregating 1s and 0s

--- Array/segregating0sand1sB.java
+++ Array/segregating0sand1sB.java
@@ -3,7 +3,7 @@ public class segregating0sand1sB {
         int[] arr = {0, 0, 1, 1, 0};
 
         int countZeros = 0;
-        int countOnes = 0;
+        
 
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]==0) countZeros++;
@@ -12,7 +12,6 @@ public class segregating0sand1sB {
 
         for (int i = 0; i < countZeros; i++) arr[i] = 0;
         for (int i = countZeros; i < arr.length; i++) arr[i] = 1;
-            
         for(int ele : arr) System.out.print(ele+ " ");
     }
 }
