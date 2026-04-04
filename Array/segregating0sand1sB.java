4a461be4b2cb36cd98f150ee87b5e07e7e2adbac
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-04-04 23:29:30 +0530

    streak restored

--- Array/segregating0sand1sB.java
+++ Array/segregating0sand1sB.java
@@ -8,6 +8,7 @@ public class segregating0sand1sB {
             if(arr[i]==0) countZeros++;
             
         }
+        
         for (int i = 0; i < countZeros; i++) arr[i] = 0;
         for (int i = countZeros; i < arr.length; i++) arr[i] = 1;
         for(int ele : arr) System.out.print(ele+ " ");
