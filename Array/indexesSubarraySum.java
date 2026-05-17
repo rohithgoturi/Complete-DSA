9a93cac3ebc2d68d229c6f8f3d78bd169feae085
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-05-18 00:14:11 +0530

    solved indexed subarray sum with optimal approach

--- Array/indexesSubarraySum.java
+++ Array/indexesSubarraySum.java
@@ -3,36 +3,59 @@ import java.util.ArrayList;
 public class indexesSubarraySum {
     public static void main(String[] args) {
 
+        ArrayList <Integer> ans = new ArrayList<>();
 
+        int [] arr = {1,2,3,7,5};
+        int target = 12;
 
-
-
-
+        int sum = 0;
+        int i = 0;
+        int j = 0;
 
         
-        //brute force approach
-        ArrayList<Integer> ans = new ArrayList<>();
-        int[] arr = {1, 2, 3, 4, 5};
-        int target = 9;
-        
-        boolean check = false;
-        for (int i = 0; i < arr.length; i++) {
-            int sum = 0;
-            for (int j = i; j < arr.length; j++) {
+        for( j=0; j < arr.length; j++){
+            sum += arr[j];
 
-                sum += arr[j];
-                if(sum==target){
-                    ans.add(i+1);
-                    ans.add(j+1);
-                    check = true;
-                    break;
-                }
+            while(sum > target && i <= j){
+                sum -= arr[i];
+                i++;
             }
-            if(check) break;
+
+            if(sum == target){
+                ans.add(i+1);
+                ans.add(j+1);
+                break;
+            }
         }
 
-        if(!check) ans.add(-1);
+        if(sum != target) ans.add(-1);
 
-        System.out.println(ans);
+        System.out.print(ans);
+
+
+        // //brute force approach
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // int[] arr = {1, 2, 3, 4, 5};
+        // int target = 9;
+        
+        // boolean check = false;
+        // for (int i = 0; i < arr.length; i++) {
+        //     int sum = 0;
+        //     for (int j = i; j < arr.length; j++) {
+
+        //         sum += arr[j];
+        //         if(sum==target){
+        //             ans.add(i+1);
+        //             ans.add(j+1);
+        //             check = true;
+        //             break;
+        //         }
+        //     }
+        //     if(check) break;
+        // }
+
+        // if(!check) ans.add(-1);
+
+        // System.out.println(ans);
     }
 }
