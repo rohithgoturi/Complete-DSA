f2f577bdecf34ffd308582bada6f65b46119e70b
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-05-22 09:49:37 +0530

    returning row with maximum sum in 2d array

---
+++ 2D ARRAYS/maxSum.java
@@ -0,0 +1,21 @@
+public class maxSum {
+    public static void main(String[] args) {
+        int arr[][] = {{2,8,3,4,7}, {7,2,1,6,3}, {5,5,4,1,4}, {3,1,8,2,6,}};
+        int maxSum = Integer.MIN_VALUE;
+        int row = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+
+            if(sum > maxSum) {
+                maxSum = sum;
+                row = i;
+            }
+        }
+
+        System.out.println("row no. " + row+1 + " has maximum sum");
+    }
+}
