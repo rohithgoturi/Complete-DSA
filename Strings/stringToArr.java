25b3b9ca6e07dd8e9c9d56f1474718206fe3a757
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-07 18:02:02 +0530

    output statement added

--- Strings/stringToArr.java
+++ Strings/stringToArr.java
@@ -2,5 +2,7 @@ public class stringToArr {
     public static void main(String[] args) {
         String str = "Hello, World!";
         char[] charArr = str.toCharArray();
+
+        System.out.println(charArr);
     }
 }
