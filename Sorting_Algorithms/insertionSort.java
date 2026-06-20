8ae38f37d212110c2562c17db607e3ba643bbee1
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-20 23:10:50 +0530

    insertion sort

---
+++ Sorting_Algorithms/insertionSort.java
@@ -0,0 +1,22 @@
+public class insertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        insertionSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
