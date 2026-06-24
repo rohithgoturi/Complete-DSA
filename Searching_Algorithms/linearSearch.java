9e70cb663f57902a18bacb26a7423c2d68128258
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-24 23:40:32 +0530

    Linear Search

---
+++ Searching_Algorithms/linearSearch.java
@@ -0,0 +1,21 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 3;
+        int result = linearSearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
