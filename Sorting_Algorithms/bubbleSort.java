d1aef0b0bc5d9bbf5a259cebf7c4c2bb3d214a2f
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-26 08:14:39 +0530

    bubble sort optimised

--- Sorting_Algorithms/bubbleSort.java
+++ Sorting_Algorithms/bubbleSort.java
@@ -10,9 +10,8 @@ public class bubbleSort {
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap elements
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
