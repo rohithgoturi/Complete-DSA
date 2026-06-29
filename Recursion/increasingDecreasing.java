7f7d120b3f9c6f5be047d638465dae2ac7e49fd5
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-30 02:39:20 +0530

    printing n to 1 and n to 1 recursively

---
+++ Recursion/increasingDecreasing.java
@@ -0,0 +1,13 @@
+public class increasingDecreasing {
+    public static void main(String[] args) {
+        int n = 5;
+        printIncDec(n);
+    }
+
+    public static void printIncDec(int n){
+        if(n==0) return;
+        System.out.print(n + " ");
+        printIncDec(n-1);
+        System.out.print(n + " ");
+    }
+}
