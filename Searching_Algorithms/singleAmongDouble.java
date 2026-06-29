ba45215270b999c64535230956025f192a220bd1
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-29 23:35:08 +0530

    finding single appearance among double using binary search approach

--- Searching_Algorithms/singleAmongDouble.java
+++ Searching_Algorithms/singleAmongDouble.java
@@ -1,13 +1,33 @@
 public class singleAmongDouble {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 2, 3, 3, 4, 5, 5};
-
-        int ans = 0;
-
-        for(int i=0; i<arr.length; i++){
-            ans = ans ^ arr[i];
-        }
-
+        // using binary search
+        int ans = findSingleAppearance(arr);
         System.out.println(ans);
+    }
+
+    public static int findSingleAppearance(int[] arr){
+        int n = arr.length;
+
+        if(n == 1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n-1] != arr[n-2]) return arr[n-1];
+        
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+            int f = mid, s = mid;
+            if(arr[mid] == arr[mid-1]) f = mid-1;
+            else s = mid+1;
+
+            int leftCount = f - low;
+            int rightCount = high - s;
+            if(leftCount%2==0) low = s + 1;
+            else high = f-1;
+
+        }
+        return -1;
     }
 }
