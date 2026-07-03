f2cad1e27316a4ce1aabcd37c704da3cc518b269
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-03 19:57:56 +0530

    adding all nodes to arraylist and displaying them

---
+++ LinkedLists/AddNodesToArrayList.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class AddNodesToArrayList {
+    public static ArrayList<Integer> printList(Node head) {
+        // code here
+        ArrayList <Integer> ans = new ArrayList<>();
+        Node temp = head;
+        
+        while(temp != null){
+            ans.add(temp.data);
+            temp = temp.next;
+        }
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        ArrayList <Integer> ans = printList(a);
+
+        System.out.println(ans);
+    }
+}
