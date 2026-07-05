f40896db27c17946db2aa9ba89eab65b23ddb039
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-05 23:28:53 +0530

    detecting whether a linked list has a loop or not

---
+++ LinkedLists/detectLoop.java
@@ -0,0 +1,34 @@
+public class detectLoop {
+    public static boolean detectLoop(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+
+    public static void main(String args[]){
+        Node a = new Node(10);
+        Node b = new Node(20);
+        a.next = b;
+        Node c = new Node(30);
+        b.next = c;
+        Node d = new Node(40);
+        c.next = d;
+        Node e = new Node(50);
+        d.next = e;
+
+        e.next = c; 
+
+        boolean hasLoop = detectLoop(a);
+        System.out.println(hasLoop);
+    }
+}
