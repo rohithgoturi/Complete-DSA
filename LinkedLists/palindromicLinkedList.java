8dc79f380aa0b4653b71af1668de4a04e012f8d6
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-06 23:32:26 +0530

    check whether linked list is palindromic list or not

---
+++ LinkedLists/palindromicLinkedList.java
@@ -0,0 +1,59 @@
+public class palindromicLinkedList {
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node second = reverse(slow.next);
+
+        Node first = head;
+
+        while (second != null) {
+            if (first.data != second.data)
+                return false;
+
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    public static Node reverse(Node head) {
+
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String args[]) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        a.next = b;
+        Node c = new Node(30);
+        b.next = c;
+        Node d = new Node(20);
+        c.next = d;
+        Node e = new Node(10);
+        d.next = e;
+
+        boolean isPalin = isPalindrome(a);
+        System.out.println(isPalin);
+    }
+}
