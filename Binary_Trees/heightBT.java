a91378e228578cc94d760c69f1ab2e89014427b9
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-24 13:34:18 +0530

    find height of binary tree

---
+++ Binary_Trees/heightBT.java
@@ -0,0 +1,17 @@
+public class heightBT {
+    public static int height(Node root) {
+		if (root == null)
+			return - 1;
+		return 1 + Math.max(height(root.left), height(root.right));
+	}
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("Height of the binary tree: " + height(root));
+    }
+}
