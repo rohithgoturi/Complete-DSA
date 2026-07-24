213e7c95ba2c8abb918c9ebcf1330f96b30d8dde
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-24 13:28:36 +0530

    get size of binary tree

---
+++ Binary_Trees/sizeBT.java
@@ -0,0 +1,34 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+public class sizeBT {
+    public static int getSize(Node root){
+        if(root == null) return 0;
+
+        int left = getSize(root.left);
+        int right = getSize(root.right);
+        int size = left + right + 1;
+
+        return size;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        System.out.println("Size of the binary tree: " + getSize(root));
+    }
+}
