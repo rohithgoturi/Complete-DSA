b9b3d136c7080b4559ff928cb99a8370137e8e79
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-05 16:11:15 +0530

    priority queue implementation for min heap

---
+++ HEAP/priorityQueue.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+public class priorityQueue {
+    public static void main(String[] args) {
+        // minHeap
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(50);
+        pq.add(30);
+        pq.add(20);
+        pq.add(40);
+
+        System.out.println(pq);
+    }
+}
