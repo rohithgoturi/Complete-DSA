b99f36fb6b7295dde6ca93bad90b516b90cf111a
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-05 16:13:22 +0530

    priority queue implementation for max Heap

--- HEAP/priorityQueue.java
+++ HEAP/priorityQueue.java
@@ -1,9 +1,13 @@
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class priorityQueue {
     public static void main(String[] args) {
         // minHeap
-        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        // PriorityQueue <Integer> pq = new PriorityQueue<>();
+        
+        //maxHeap
+        PriorityQueue <Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         pq.add(10);
         pq.add(50);
         pq.add(30);
