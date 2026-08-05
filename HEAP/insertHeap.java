3abfef318aefb0555ac04576947c48d4cfffb1f4
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-05 19:17:07 +0530

    implementaion of heap and its methods using ArrayList

--- HEAP/insertHeap.java
+++ HEAP/insertHeap.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
 
 public class insertHeap {
     static class Heap{
@@ -10,13 +9,13 @@ public class insertHeap {
             int x = ans.size()-1;
             int par = (x-1)/2;
 
-            while(ans.get(x) < ans.get(par)){
+            while(x > 0 && ans.get(x) < ans.get(par)){
                 int temp = ans.get(x);
                 ans.set(x, ans.get(par));
                 ans.set(par, temp);
 
                 x = par;
-                par = x-1/2;
+                par = (x-1)/2;
             }
         }
 
@@ -29,11 +28,11 @@ public class insertHeap {
             int right = 2*idx+2;
             int minIdx = idx;
 
-            if(left < ans.size() && ans.get(idx) < ans.get(left)){
+            if(left < ans.size() && ans.get(left) < ans.get(minIdx)){
                 minIdx = left;
             }
 
-            if(right < ans.size() && ans.get(idx) < ans.get(right)){
+            if(right < ans.size() && ans.get(right) < ans.get(minIdx)){
                 minIdx = right;
             }
 
@@ -54,6 +53,8 @@ public class insertHeap {
             ans.set(0, ans.get(ans.size()-1));
             ans.set(ans.size()-1, temp);
 
+            ans.remove(ans.size()-1);
+
             heapify(0);
 
             return data;
@@ -62,5 +63,19 @@ public class insertHeap {
         public boolean isEmpty(){
             return ans.size() == 0;
         }
+    }
+
+    public static void main(String[] args) {
+        Heap h = new Heap();
+        h.insert(3);
+        h.insert(5);
+        h.insert(1);
+
+        while(!h.isEmpty()){
+            System.out.println(h.peek());
+            h.remove();
+        }
+
+        System.out.println(h.isEmpty());
     }
 }
