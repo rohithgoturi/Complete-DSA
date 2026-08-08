eb39d80cf897f3c0d3bdae758266d767fd406d6c
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-08 23:45:02 +0530

    Implement basic hashing(hashmap)

---
+++ Hashing/hashmap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("India", 01);
+        map.put("China", 02);
+        map.put("USA", 03);
+
+        System.out.println(map);
+    }
+}
