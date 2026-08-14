5febe62207a505875c00cd13aca3f52a86e0755f
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-15 00:13:58 +0530

    surpassing unchecked warning and displaying the graph created

--- Graphs/basicsGraph.java
+++ Graphs/basicsGraph.java
@@ -1,45 +1,56 @@
 import java.util.ArrayList;
 
 public class basicsGraph {
-    static class Edges{
+    static class Edges {
         int src;
         int dest;
         int wt;
 
-        public Edges(int s, int d, int w){
+        public Edges(int s, int d, int w) {
             this.src = s;
             this.dest = d;
             this.wt = w;
         }
     }
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         int V = 5;
-        ArrayList<Edges> [] graph = new ArrayList[V];
-        
+        ArrayList<Edges>[] graph = new ArrayList[V];
+
         for (int i = 0; i < V; i++) {
             graph[i] = new ArrayList<>();
         }
 
         // vertex 0
-        graph[0].add(new Edges(0,1,5));
+        graph[0].add(new Edges(0, 1, 5));
 
-        //vertex 1
-        graph[1].add(new Edges(1,2,1));
-        graph[1].add(new Edges(1,3,2));
-        graph[1].add(new Edges(1,0,5));
+        // vertex 1
+        graph[1].add(new Edges(1, 2, 1));
+        graph[1].add(new Edges(1, 3, 2));
+        graph[1].add(new Edges(1, 0, 5));
 
-        //vertex 2
-        graph[2].add(new Edges(2,1,1));
-        graph[2].add(new Edges(2,3,1));
-        graph[2].add(new Edges(2,4,2));
+        // vertex 2
+        graph[2].add(new Edges(2, 1, 1));
+        graph[2].add(new Edges(2, 3, 1));
+        graph[2].add(new Edges(2, 4, 2));
 
-        //vertex 3
-        graph[3].add(new Edges(3,2,1));
-        graph[3].add(new Edges(3,1,3));
+        // vertex 3
+        graph[3].add(new Edges(3, 2, 1));
+        graph[3].add(new Edges(3, 1, 3));
 
-        //vertex 4
-        graph[4].add(new Edges(4,2,2));
+        // vertex 4
+        graph[4].add(new Edges(4, 2, 2));
+
+        for (int i = 0; i < V; i++) {
+            System.out.print("Vertex " + i + " -> ");
+
+            for (Edges e : graph[i]) {
+                System.out.print("(" + e.dest + ", wt=" + e.wt + ") ");
+            }
+
+            System.out.println();
+        }
 
     }
 }
