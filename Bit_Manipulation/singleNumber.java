c102d820be052d38d5d6e4efdfaa709752ff8d50
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-15 23:14:49 +0530

    finding single occurance number

---
+++ Bit_Manipulation/singleNumber.java
@@ -0,0 +1,15 @@
+public class singleNumber {
+    public static int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        int result = singleNumber(nums);
+        System.out.println("The single number is: " + result);
+    }
+}
