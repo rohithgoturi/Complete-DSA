b7e6c10dd39458fafd6a31f7104fb83d305bec5e
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-21 22:53:51 +0530

    Safe states

--- Graphs/safeStates.java
+++ Graphs/safeStates.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class dijkstra {
+public class safeStates {
     public static ArrayList<Integer> safeNodes(int V, int[][] edges) {
 
         List<List<Integer>> list = new ArrayList<>();
